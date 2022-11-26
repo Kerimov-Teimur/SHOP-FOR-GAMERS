47d978a2af48f156d275e5bd873c150c2d4dcec2
Kerimov-Teimur <113027418+Kerimov-Teimur@users.noreply.github.com>
2022-11-26 09:24:46 +0000

    Deploying to gh-pages from @ Kerimov-Teimur/SHOP-FOR-GAMERS@7ab5df75f595c939bd197d9719596f748c311920 🚀

---
+++ accessories-page.2542f3a6.js
@@ -0,0 +1,2 @@
+$(".one-time").slick({dots:!0,infinite:!0,speed:300,slidesToShow:1,adaptiveHeight:!0,arrows:!1});
+//# sourceMappingURL=accessories-page.2542f3a6.js.map
--- accessories-page.78e55b3d.js
+++
@@ -1,2 +0,0 @@
-$(".one-time").slick({dots:!0,infinite:!0,speed:300,slidesToShow:1,adaptiveHeight:!0,arrows:!1,autoplay:!0,autoplay:1e3});
-//# sourceMappingURL=accessories-page.78e55b3d.js.map
